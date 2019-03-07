a3ecfaa55b9a3527c3167ea5365a4da6d938b639
JacyL4 <jacyl4@gmail.com>
2019-03-08 00:49:29 +0800

    Update pdvserver-LNMP.sh

--- pdvserver/pdvserver-LNMP.sh
+++ pdvserver/pdvserver-LNMP.sh
@@ -188,13 +188,7 @@ location $v2path {
   proxy_set_header X-Forwarded-Proto \$scheme;
   proxy_set_header X-Real-IP \$remote_addr;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_intercept_errors on;
   proxy_read_timeout 86400;
-  proxy_buffer_size 512k;
-  proxy_buffers 8 512k;
-  proxy_busy_buffers_size 512k;
-  proxy_temp_file_write_size 512k;
-  proxy_max_temp_file_size 128m;
 }
 
   location ~ /.well-known {
