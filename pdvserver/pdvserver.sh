85bf69734c23ca239c73b5f416c8ae357254d9f1
JacyL4 <jacyl4@gmail.com>
2019-03-08 13:00:02 +0800

    Update pdvserver.sh

--- pdvserver/pdvserver.sh
+++ pdvserver/pdvserver.sh
@@ -195,7 +195,7 @@ location /dq {
   proxy_set_header X-Forwarded-Proto \$scheme;
   proxy_set_header X-Real-IP \$remote_addr;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_read_timeout 600;
+  proxy_read_timeout 86400;
 }
 
 location $v2path {
