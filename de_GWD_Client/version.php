47347cd7e83cf752226997e73119ade309934674
JacyL4 <jacyl4@gmail.com>
2019-05-06 02:53:28 +0800

    Update version.php

--- de_GWD_Client/version.php
+++ de_GWD_Client/version.php
@@ -1,1 +1,7 @@
-1.0.4
+1.0.5
+-
+<?php 
+$str= file_get_contents('https://raw.githubusercontent.com/jacyl4/de_GWD/master/de_GWD_Client/version.php');
+$array=explode('/', $str);
+echo $array[0];
+?>
