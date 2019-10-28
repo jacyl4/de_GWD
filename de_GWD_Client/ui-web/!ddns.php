e3717c1c7ab058c3be92c4023de635bfc5afdb66
gwdburst <jacyl4@me.com>
2019-10-29 02:29:32 +0800

    auto

--- de_GWD_Client/ui-web/!ddns.php
+++ de_GWD_Client/ui-web/!ddns.php
@@ -170,7 +170,7 @@
 <div class="form-row mb-3">
       <div class="col-md-6 input-group">
         <div class="input-group-prepend w-25">
-          <span class="input-group-text justify-content-center w-100">地址</span>
+          <span class="input-group-text justify-content-center w-100">域名/公网IP</span>
         </div>
           <input type="text" id="WGaddress" class="form-control" value="<?php echo exec("awk 'NR==1{print}' /var/www/html/WGaddress.txt"); ?>">
       </div>
