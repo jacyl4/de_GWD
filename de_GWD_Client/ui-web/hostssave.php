e6f54ddf4fc7b6b609cbb9229f41ec474fcf14a3
jacyl4 <jacyl4@gmail.com>
2019-11-04 04:56:01 +0800

    auto

--- de_GWD_Client/ui-web/hostssave.php
+++ de_GWD_Client/ui-web/hostssave.php
@@ -1,13 +1,7 @@
 <?php require_once('auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-$hostsdefault = $_GET['hostsdefault'];
 $hostscustomize = $_GET['hostscustomize'];
-
-$hostsdefaulttxt = fopen("hostsdefault.txt", "w");
-$txt = "$hostsdefault\n";
-fwrite($hostsdefaulttxt, $txt);
-fclose($hostsdefaulttxt);
 
 $hostscustomizetxt = fopen("hostscustomize.txt", "w");
 $txt = "$hostscustomize\n";
