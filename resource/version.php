1d9d3182cc448db7f388911376005e983fa5dfdd
jacyl4 <jacyl4@gmail.com>
2019-11-11 05:55:54 +0800

    auto

--- resource/version.php
+++ resource/version.php
@@ -1,7 +1,7 @@
 1.6.45
 -
 <?php
-$str= file_get_contents('https://raw.githubusercontent.com/jacyl4/de_GWD/master/de_GWD_Client/version.php');
+$str= file_get_contents('https://raw.githubusercontent.com/jacyl4/de_GWD/master/resource/version.php');
 $array=explode('/', $str);
 echo $array[0];
 ?>
