4f358c746c8d9d0586e76e627f0bb222ac09a705
JacyL4 <jacyl4@gmail.com>
2020-01-09 08:42:01 +0800

    auto

--- resource/ui-web/proxyon.php
+++
@@ -1,6 +0,0 @@
-<?php require_once('auth.php'); ?>
-<?php if (isset($auth) && $auth) {?>
-<?php
-shell_exec('sudo systemctl restart iptables-proxy');
-?>
-<?php }?>
--- resource/ui-web/wgqrtxt6.php
+++
@@ -1,6 +0,0 @@
-<?php require_once('auth.php'); ?>
-<?php if (isset($auth) && $auth) {?>
-<?php
-passthru('sudo cat /etc/wireguard/client6.conf');
-?>
-<?php }?>
