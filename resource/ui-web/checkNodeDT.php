ffa9503ffe17c341d53a9981dc878937817a3e45
JacyL4 <jacyl4@gmail.com>
2020-02-03 20:12:31 +0800

    auto

--- resource/ui-web/checkNodeDT.php
+++ resource/ui-web/checkNodeDT.php
@@ -1,11 +1,9 @@
 <?php require_once('auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-
 $nodedtnum = exec("/usr/local/bin/ui-checkNodeDT"); 
 
-echo shell_exec("awk 'NR=={$nodedtnum}{print}' /var/www/html/nodename.txt");
+echo json_decode(file_get_contents('/usr/local/bin/0conf'))->v2node[$nodedtnum]->name;
 
-die();
 ?>
 <?php }?>
--- resource/ui-web/checkNodeNF.php
+++ resource/ui-web/checkNodeNF.php
@@ -1,11 +1,9 @@
 <?php require_once('auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-
 $nodenfnum = exec("/usr/local/bin/ui-checkNodeNF"); 
 
-echo shell_exec("awk 'NR=={$nodenfnum}{print}' /var/www/html/nodename.txt");
+echo json_decode(file_get_contents('/usr/local/bin/0conf'))->v2node[$nodenfnum]->name;
 
-die();
 ?>
 <?php }?>
