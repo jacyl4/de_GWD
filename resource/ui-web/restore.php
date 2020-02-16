9ddca3df4db2520c0ac098f35c032d36f940cd38
JacyL4 <jacyl4@gmail.com>
2020-02-17 06:20:33 +0800

    auto

--- resource/ui-web/restore.php
+++ resource/ui-web/restore.php
@@ -6,13 +6,15 @@ if ($_FILES["file"]["name"] == "0conf")
 move_uploaded_file($_FILES['file']['tmp_name'], '' . $_FILES['file']['name']);
 }
 exec('sudo cp -f /var/www/html/0conf /usr/local/bin/');
+
+if (!empty(json_decode(file_get_contents('/usr/local/bin/0conf'))->address->alias))
+{
+exec('sudo /usr/local/bin/ui-markThis');
+}
 exec('sudo /usr/local/bin/ui-hostSave');
 exec('sudo /usr/local/bin/ui-nodeResolve');
 exec('sudo /usr/local/bin/ui-changeDOH');
 exec('sudo chmod 666 /usr/local/bin/0conf');
+
 ?>
 <?php }?>
-
-
-
-
