0b406bcd55b5d89175ad74082e94cf98ff85c524
JacyL4 <jacyl4@gmail.com>
2020-02-24 06:33:51 +0800

    auto

--- resource/ui-web/dhcpDOWN.php
+++ resource/ui-web/dhcpDOWN.php
@@ -1,6 +1,11 @@
 <?php require_once('auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
+$data = json_decode(file_get_contents('/usr/local/bin/0conf'), true);
+$data['address']['oh'] = "";
+$newJsonString = json_encode($data, JSON_PRETTY_PRINT);
+file_put_contents('/usr/local/bin/0conf', $newJsonString);
+
 exec('sudo pihole -a disabledhcp');
 ?>
 <?php }?>
