370ca279ac9149c5ec3f913639c4b38dbbd48c64
JacyL4 <jacyl4@gmail.com>
2020-02-24 08:53:34 +0800

    auto

--- resource/ui-web/dhcpDOWN.php
+++ resource/ui-web/dhcpDOWN.php
@@ -2,7 +2,7 @@
 <?php if (isset($auth) && $auth) {?>
 <?php
 $data = json_decode(file_get_contents('/usr/local/bin/0conf'), true);
-$data['address']['oh'] = "";
+$data['address']['dhcp'] = "";
 $newJsonString = json_encode($data, JSON_PRETTY_PRINT);
 file_put_contents('/usr/local/bin/0conf', $newJsonString);
 
