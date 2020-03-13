e7586a22853032c3a41a344f87d2cabb7797c374
JacyL4 <jacyl4@gmail.com>
2020-03-14 06:45:29 +0800

    auto

--- resource/ui-web/ping1.php
+++ resource/ui-web/ping1.php
@@ -1,4 +1,5 @@
 <?php
-echo shell_exec("ping -n -c1 -w1 $(jq -r '.v2node[0].domain' /usr/local/bin/0conf | cut -d : -f1) 2>&1 | grep 'time=' | cut -d = -f 4 | cut -d' ' -f 1");
+putenv("nodeNUM=1");
+echo shell_exec('/usr/local/bin/pingNODE $nodeNUM');
 die();
 ?>
--- resource/ui-web/pingDOH2.php
+++ resource/ui-web/pingDOH2.php
@@ -1,4 +1,5 @@
 <?php
-echo shell_exec("ping -n -c1 -w1 $(jq -r '.doh.doh2' /usr/local/bin/0conf | cut -d : -f1) 2>&1 | grep 'time=' | cut -d = -f 4 | cut -d' ' -f 1");
+putenv("dohNUM=2");
+echo shell_exec('/usr/local/bin/pingDOH $dohNUM');
 die();
 ?>
