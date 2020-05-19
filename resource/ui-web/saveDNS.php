297a917bb9316ca861ca022eed5c255b1bc72aaa
JacyL4 <jacyl4@gmail.com>
2020-05-20 05:47:44 +0800

    auto

--- resource/ui-web/saveDNS.php
+++ resource/ui-web/saveDNS.php
@@ -13,8 +13,8 @@ $newJsonString = json_encode($data, JSON_PRETTY_PRINT);
 file_put_contents('/usr/local/bin/0conf', $newJsonString);
 
 $data = json_decode(file_get_contents('/usr/local/bin/0conf'), true);
-$data['doh']['doh1'] = $DoH1;
-$data['doh']['doh2'] = $DoH2;
+$data['dns']['doh1'] = $DoH1;
+$data['dns']['doh2'] = $DoH2;
 $newJsonString = json_encode($data, JSON_PRETTY_PRINT);
 file_put_contents('/usr/local/bin/0conf', $newJsonString);
 
