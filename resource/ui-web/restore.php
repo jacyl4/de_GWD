56e4d51220ddbc03dc389994219a4c150a35e590
JacyL4 <jacyl4@gmail.com>
2020-07-02 15:59:16 +0800

    auto

--- resource/ui-web/restore.php
+++ resource/ui-web/restore.php
@@ -3,40 +3,10 @@
 <?php
 if ($_FILES["file"]["name"] == "0conf")
 {
-move_uploaded_file($_FILES['file']['tmp_name'], 'restore/' . $_FILES['file']['name']);
+	move_uploaded_file($_FILES['file']['tmp_name'], 'restore/' . $_FILES['file']['name']);
 }
-exec('sudo cp -f /var/www/html/restore/0conf /usr/local/bin/');
+exec('sudo mv -f /var/www/html/restore/0conf /usr/local/bin/0conf');
 
-exec('sudo /usr/local/bin/ui-restorePW');
-exec('sudo /usr/local/bin/ui-saveDNSChina');
-exec('sudo /usr/local/bin/ui-NodeDThide');
-
-if ( json_decode(file_get_contents('/usr/local/bin/0conf'))->address->udp === "on" ){
-  	exec('sudo /usr/local/bin/ui-udpON');
-} else {
-  	exec('sudo /usr/local/bin/ui-udpOFF');
-}
-
-exec('sudo systemctl restart smartdns');
-exec('sudo systemctl restart doh-client');
-exec('sudo systemctl restart v2dns');
-exec('sudo systemctl restart vtrui');
-exec('sudo systemctl restart iptables-proxy');
-
-exec('sudo /usr/local/bin/ui_4h');
-
-if (json_decode(file_get_contents('/usr/local/bin/0conf'))->address->dhcp === "on")
-{
-exec('sudo /usr/local/bin/ui-dhcpUP');
-} else {
-exec('sudo pihole -a disabledhcp');
-}
-
-if (!empty(json_decode(file_get_contents('/usr/local/bin/0conf'))->address->alias))
-{
-exec('sudo /usr/local/bin/ui-markThis');
-}
-
-exec('sudo /usr/local/bin/ui-updateGen');
+exec('sudo /usr/local/bin/ui-restore');
 ?>
 <?php }?>
