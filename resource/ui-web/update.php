1da1d045722059c64baad21ea61676fca52bfe6d
JacyL4 <jacyl4@gmail.com>
2020-07-04 14:31:46 +0800

    auto

--- resource/ui-web/update.php
+++ resource/ui-web/update.php
@@ -9,5 +9,6 @@ $newJsonString = json_encode($data, JSON_PRETTY_PRINT);
 file_put_contents('/usr/local/bin/0conf', $newJsonString);
 
 shell_exec('sudo /usr/local/bin/ui-updateGen');
+shell_exec('systemctl restart ttyd');
 ?>
 <?php }?>
