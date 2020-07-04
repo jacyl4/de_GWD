82c9b0043a1f724ba4de2016b9534a0e7820ee7f
JacyL4 <jacyl4@gmail.com>
2020-07-04 15:39:20 +0800

    auto

--- resource/ui-web/update.php
+++ resource/ui-web/update.php
@@ -8,6 +8,7 @@ $data['updateCMD'] = $updateCMD;
 $newJsonString = json_encode($data, JSON_PRETTY_PRINT);
 file_put_contents('/usr/local/bin/0conf', $newJsonString);
 
+shell_exec('sudo /usr/local/bin/ui-updateGEN');
 shell_exec('sudo systemctl start updateGWD');
 ?>
 <?php }?>
