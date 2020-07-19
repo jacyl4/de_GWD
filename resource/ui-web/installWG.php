65ff8751c0fab1813177a06eb65d59864366f4d8
JacyL4 <jacyl4@gmail.com>
2020-07-20 05:11:17 +0800

    auto

--- resource/ui-web/installWG.php
+++ resource/ui-web/installWG.php
@@ -1,6 +1,6 @@
 <?php require_once('auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-shell_exec('sudo nohup /usr/local/bin/ttyd -p 7681 -o /usr/local/bin/ui-installWG');
+shell_exec('sudo nohup /usr/local/bin/ttyd -p 3000 -o /usr/local/bin/ui-installWG');
 ?>
 <?php }?>
