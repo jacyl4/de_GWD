6fcdb7d1f4925485392d00c198b64a16151e0d29
JacyL4 <jacyl4@gmail.com>
2020-11-13 09:16:13 +0800

    auto

--- resource/ui-web/act/restartProxy.php
+++ resource/ui-web/act/restartProxy.php
@@ -6,5 +6,6 @@ shell_exec('sudo systemctl restart doh-client');
 shell_exec('sudo systemctl restart v2dns');
 shell_exec('sudo systemctl restart vtrui');
 shell_exec('sudo systemctl restart iptables-proxy');
+shell_exec('sudo systemctl restart AdGuardHome');
 ?>
 <?php }?>
