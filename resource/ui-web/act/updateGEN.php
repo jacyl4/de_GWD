f0e976c37ca9b571da7fe297c875ac4d7f67fce9
JacyL4 <jacyl4@gmail.com>
2020-12-03 07:09:01 +0800

    auto

--- resource/ui-web/act/updateGEN.php
+++ resource/ui-web/act/updateGEN.php
@@ -1,17 +1,19 @@
 <?php require_once('../auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-$conf = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
 $updateCMD = $_GET['updateCMD'];
 
+$conf = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
 $conf['updateCMD'] = $updateCMD;
 $newJsonString = json_encode($conf, JSON_PRETTY_PRINT);
 file_put_contents('/opt/de_GWD/0conf', $newJsonString);
 
-file_put_contents('ui-update', "#!/bin/bash\n/usr/bin/ttyd -p 3000 -o $updateCMD");
+$updateURL = strpos($updateCMD,"https");
+$updateURL = substr($updateCMD, $updateURL);
+$updateURL = substr($updateURL, 0, strlen($updateURL)-1);
 
-shell_exec('sudo mv -f /var/www/html/act/ui-update /opt/de_GWD/ui-update');
-shell_exec('sudo chmod +x /opt/de_GWD/ui-update');
+shell_exec("sudo wget --no-check-certificate -qO /opt/de_GWD/update $updateURL");
+shell_exec('sudo chmod +x /opt/de_GWD/update');
 shell_exec('sudo systemctl start updateGWD');
 ?>
 <?php }?>
