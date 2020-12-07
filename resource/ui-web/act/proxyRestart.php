ba4c3852c0f5595d8f207fcf234d0f1990f4c364
JacyL4 <jacyl4@gmail.com>
2020-12-08 05:04:23 +0800

    auto

--- resource/ui-web/act/proxyRestart.php
+++ resource/ui-web/act/proxyRestart.php
@@ -3,8 +3,6 @@
 <?php
 shell_exec('sudo systemctl restart smartdns');
 shell_exec('sudo systemctl restart v2dns');
-shell_exec('sudo systemctl restart vtrui');
-shell_exec('sudo systemctl restart iptables-proxy');
 shell_exec('sudo yq w -i /opt/AdGuardHome/AdGuardHome.yaml dns.upstream_dns[0] "127.0.0.1:5350"');
 shell_exec('sudo yq w -i /opt/AdGuardHome/AdGuardHome.yaml dns.bootstrap_dns[0] "127.0.0.1:5350"');
 shell_exec('sudo systemctl restart AdGuardHome');
@@ -17,5 +15,9 @@ shell_exec('sudo cat /dev/null > /etc/resolvconf/resolv.conf.d/tail');
 shell_exec('sudo echo "nameserver 127.0.0.1" > /etc/resolvconf/resolv.conf.d/base');
 shell_exec('sudo ln -fs /etc/resolvconf/run/resolv.conf /etc/resolv.conf');
 shell_exec('sudo resolvconf -u');
+
+shell_exec('sudo /opt/de_GWD/ui_4h');
+shell_exec('sudo systemctl restart vtrui');
+shell_exec('sudo systemctl restart iptables-proxy');
 ?>
 <?php }?>
