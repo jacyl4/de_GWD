4bde77e93dfa3af52e1b18421eb9e13ecd922d6d
JacyL4 <jacyl4@gmail.com>
2020-12-11 02:08:21 +0800

    auto

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 1.11.13
 -
 <?php
-$str= file_get_contents('https://github.com/jacyl4/de_GWD/raw/main/version.php');
+$str= file_get_contents('https://raw.githubusercontent.com/jacyl4/de_GWD/main/version.php');
 $array=explode('/', $str);
 echo $array[0];
 ?>
