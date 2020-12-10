f21eda59eb3fbff7129762c4857510fe04155b73
JacyL4 <jacyl4@gmail.com>
2020-12-11 07:53:36 +0800

    auto

--- resource/ui-web/act/dnsGFW.php
+++ resource/ui-web/act/dnsGFW.php
@@ -2,7 +2,6 @@
 <?php if (isset($auth) && $auth) {?>
 <?php 
 shell_exec('sudo /opt/de_GWD/ui-dnsGFW');
-shell_exec('sudo /opt/de_GWD/ui-saveListBW');
 shell_exec('sudo systemctl restart v2dns');
 shell_exec('sudo systemctl restart iptables-proxy');
 shell_exec('sudo /opt/AdGuardHome/AdGuardHome -s reload');
