67c89656394768d2b8ea8bec52127b46c8610f57
JacyL4 <jacyl4@gmail.com>
2021-02-16 16:23:23 +0800

    auto

--- resource/ui-web/act/arrNFS.php
+++ resource/ui-web/act/arrNFS.php
@@ -1,6 +1,6 @@
 <?php require_once('../auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-echo exec("sudo nfsstat -m | sed '/Flags/d' | sed '/^\s*$/d'");
+system("sudo nfsstat -m | sed '/Flags/d' | sed '/^\s*$/d'");
 ?>
 <?php }?>
