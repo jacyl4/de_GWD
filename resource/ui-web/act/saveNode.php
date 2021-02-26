c597268a0dc8b8af24c99127ee944c03b1bdcf3b
JacyL4 <jacyl4@gmail.com>
2021-02-27 00:53:27 +0800

    auto

--- resource/ui-web/act/saveNode.php
+++ resource/ui-web/act/saveNode.php
@@ -9,7 +9,6 @@ $newJsonString = json_encode($conf, JSON_PRETTY_PRINT);
 file_put_contents('/opt/de_GWD/0conf', $newJsonString);
 
 shell_exec('sudo /opt/de_GWD/ui_4h');
-shell_exec('sudo systemctl restart smartdns');
 shell_exec('sudo systemctl restart v2dns');
 shell_exec('sudo systemctl restart iptables-proxy');
 shell_exec('sudo pihole -f');
