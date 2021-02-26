d30e6e2cafcedfaaebefc358ed2ad95ca9c83795
JacyL4 <jacyl4@gmail.com>
2021-02-26 08:37:43 +0800

    auto

--- resource/ui-web/act/installNetdata.php
+++ resource/ui-web/act/installNetdata.php
@@ -1,6 +1,7 @@
 <?php require_once('../auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
+ini_set('max_execution_time', '0');
 shell_exec('sudo nohup /usr/bin/ttyd -p 3000 -o /opt/de_GWD/ui-installNetdata &');
 ?>
 <?php }?>
