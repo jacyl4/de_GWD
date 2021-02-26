4889eece6895c11cec3f35b9dca6c0b431cc8e9f
JacyL4 <jacyl4@gmail.com>
2021-02-26 09:26:44 +0800

    auto

--- resource/ui-web/app.php
+++ resource/ui-web/app.php
@@ -145,7 +145,9 @@
   </div>
 </div>
 
-<button id="installNetdata" type="button" class="btn btn-outline-secondary btn-sm mb-3" style="border-Radius: 0px;">install Netdata</button>
+<div class="form-row col-md-12">
+<button id="installNetdata" type="button" class="btn btn-outline-secondary btn-sm" style="border-Radius: 0px;">install Netdata</button>
+</div>
 
         <!-- Page Content -->      
         <div class="card mb-3">
