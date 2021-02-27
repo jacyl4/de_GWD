6b3929adbcb408b708241b79627d43df980c7681
JacyL4 <jacyl4@gmail.com>
2021-02-27 10:16:54 +0800

    auto

--- resource/ui-web/act/switchNodeDT.php
+++ resource/ui-web/act/switchNodeDT.php
@@ -5,13 +5,12 @@ $switchNodeDT = $_GET['switchNodeDT'];
 $forward0 = exec('sudo [ -f /etc/nginx/conf.d/forward0.conf ] && echo installed');
 
 if ($switchNodeDT == "NodeDTshow"){
-    exec('sudo /opt/de_GWD/ui-NodeDTshow');
+    exec('sudo /opt/de_GWD/ui-NodeDTshow r');
 }
 elseif ($switchNodeDT == "NodeDThide"){
-    exec('sudo /opt/de_GWD/ui-NodeDThide');
+    exec('sudo /opt/de_GWD/ui-NodeDThide r');
 }
 
-if ($forward0 == installed)	exec('sudo /opt/de_GWD/ui-FWD0vtrui');
-exec('sudo systemctl restart vtrui');
+if ($forward0 == installed)	exec('sudo /opt/de_GWD/ui-FWD0save r');
 ?>
 <?php }?>
