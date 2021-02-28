3cfea517e1b8eb386679fe4dba7e12c338a2fc56
JacyL4 <jacyl4@gmail.com>
2021-02-28 21:54:18 +0800

    auto

--- resource/ui-web/act/checkCER.php
+++ resource/ui-web/act/checkCER.php
@@ -1,8 +1,6 @@
 <?php require_once('../auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-$filePath = exec('sudo [ -f /var/www/ssl/ocsp.resp ] && echo installed');
-
-if ($filePath == installed) echo 'installed';
+passthru('sudo [ -f /var/www/ssl/ocsp.resp ] && echo installed');
 ?>
 <?php }?>
