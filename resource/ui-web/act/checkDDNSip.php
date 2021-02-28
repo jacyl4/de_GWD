878ce1f80ef055fbbd2b6f4072b6f3cad1c36d4b
JacyL4 <jacyl4@gmail.com>
2021-03-01 02:59:13 +0800

    auto

--- resource/ui-web/act/checkDDNSip.php
+++ resource/ui-web/act/checkDDNSip.php
@@ -1,4 +1,6 @@
 <?php
-passthru('curl http://members.3322.org/dyndns/getip');
+$wanIP = exec('curl http://members.3322.org/dyndns/getip');
+
+if(filter_var($wanIP, FILTER_VALIDATE_IP)) echo $wanIP;
 die();
 ?>
