fa8bfed65db6b6cff10d5860c414491017a619c5
JacyL4 <jacyl4@gmail.com>
2021-08-29 03:59:30 +0800

    Init

---
+++ resource/client/ui-web/act/markThis.php
@@ -0,0 +1,13 @@
+<?php require_once('../auth.php'); ?>
+<?php if (isset($auth) && $auth) {?>
+<?php
+$conf = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
+$markThis = $_GET['markName'];
+
+$conf['address']['alias'] = $markThis;
+$newJsonString = json_encode($conf, JSON_PRETTY_PRINT);
+file_put_contents('/opt/de_GWD/0conf', $newJsonString);
+
+exec('sudo /opt/de_GWD/ui-markThis >/dev/null 2>&1 &');
+?>
+<?php }?>
