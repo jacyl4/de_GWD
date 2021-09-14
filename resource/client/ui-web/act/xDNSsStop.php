fe086d8054078159873c3a28183bbad0dcd14f5d
JacyL4 <jacyl4@gmail.com>
2021-09-14 20:17:13 +0800

    Init

---
+++ resource/client/ui-web/act/xDNSsStop.php
@@ -0,0 +1,11 @@
+<?php require_once('../auth.php'); ?>
+<?php if (isset($auth) && $auth) {?>
+<?php
+$data = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
+$data['FORWARD']['xDNSs']['status'] = "off";
+$newJsonString = json_encode($data, JSON_PRETTY_PRINT);
+file_put_contents('/opt/de_GWD/0conf', $newJsonString);
+
+exec('sudo /opt/de_GWD/ui-xDNSsStop &');
+?>
+<?php }?>
