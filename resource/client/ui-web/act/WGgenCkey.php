3b130046d6ca699370b4aa5509756f8d29e4fb3e
JacyL4 <jacyl4@gmail.com>
2021-10-28 00:51:10 +0800

    Init

---
+++ resource/client/ui-web/act/WGgenCkey.php
@@ -0,0 +1,13 @@
+<?php require_once('../auth.php'); ?>
+<?php if (isset($auth) && $auth) {?>
+<?php
+$WGnum = $_GET['WGnum'];
+$conf = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
+
+$WGSkey = $conf->wireguard->server->sprivatekey;
+
+if (empty($WGSkey) === true || $WGSkey == null) exec("sudo /opt/de_GWD/ui-WGgenSkey");
+
+exec("sudo /opt/de_GWD/ui-WGgenCkey $WGnum");
+?>
+<?php }?>
