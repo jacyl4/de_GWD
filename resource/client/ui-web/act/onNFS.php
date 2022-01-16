d3a2159d788e6b867363059a940e9ce3c83fc66b
jacyl4 <jacyl4@gmail.com>
2022-01-16 11:56:15 +0800

    Init

---
+++ resource/client/ui-web/act/onNFS.php
@@ -0,0 +1,14 @@
+<?php require_once('../auth.php'); ?>
+<?php if (isset($auth) && $auth) {?>
+<?php
+$conf = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
+$NFSlist = $_GET['NFSlist'];
+
+unset($conf['app']['NFS']);
+$conf['app']['NFS'] = $NFSlist;
+$newJsonString = json_encode($conf, JSON_PRETTY_PRINT);
+file_put_contents('/opt/de_GWD/0conf', $newJsonString);
+
+exec('sudo /opt/de_GWD/ui-onNFS &');
+?>
+<?php }?>
