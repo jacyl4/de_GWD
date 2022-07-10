da2d24dc700bf8e032db23a5564a396e70ccb295
jacyl4 <jacyl4@gmail.com>
2022-07-11 00:37:45 +0800

    Init

---
+++ resource/client/ui-web/act/DoGsSave.php
@@ -0,0 +1,15 @@
+<?php require_once('../auth.php'); ?>
+<?php if (isset($auth) && $auth) {?>
+<?php
+$DoGsPort = $_GET['DoGsPort'];
+
+$data = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
+$data['FORWARD']['DoGs'] = array();
+$data['FORWARD']['DoGs']['status'] = "on";
+$data['FORWARD']['DoGs']['port'] = $DoGsPort;
+$newJsonString = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+file_put_contents('/opt/de_GWD/0conf', $newJsonString);
+
+exec('sudo /opt/de_GWD/ui-DoGsSave &');
+?>
+<?php }?>
