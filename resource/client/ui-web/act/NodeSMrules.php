2d3053605e772953b97d4a64bf9ffd757a1ce879
jacyl4 <jacyl4@gmail.com>
2022-07-17 09:47:19 +0800

    Init

---
+++ resource/client/ui-web/act/NodeSMrules.php
@@ -0,0 +1,12 @@
+<?php require_once('../auth.php'); ?>
+<?php if (isset($auth) && $auth) {?>
+<?php
+$nodeSMshowYoutube = $_GET['nodeSMshowYoutube'];
+$nodeSMshowNetflix = $_GET['nodeSMshowNetflix'];
+$nodeSMshowHDH = $_GET['nodeSMshowHDH'];
+$nodeSMshowTVB = $_GET['nodeSMshowTVB'];
+$nodeSMshowBahamut = $_GET['nodeSMshowBahamut'];
+$nodeSMshowApple = $_GET['nodeSMshowApple'];
+exec("sudo /opt/de_GWD/ui-NodeSM r $nodeSMshowYoutube $nodeSMshowNetflix $nodeSMshowHDH $nodeSMshowTVB $nodeSMshowBahamut $nodeSMshowApple");
+?>
+<?php }?>
